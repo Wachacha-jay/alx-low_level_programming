197666d96f7c4149d6e5ac8ac7849f8fe97a7216
Wachacha-jay <jameswachacha@gmail.com>
2022-09-11 21:25:22 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * print single digit number of base 10
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+int a = 0;
+for (; a < 10 ; a++)
+putchar('0' + a);
+putchar('\n');
+return (0);
+}
