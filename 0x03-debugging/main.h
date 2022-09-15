e046f93872be967fed1a1bb2087aadaa2d925ea9
Wachacha-jay <jameswachacha@gmail.com>
2022-09-15 08:54:22 -0700

    leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -21,4 +21,35 @@ void positive_or_negative(int i);
  * Return: largest number
  */
 int largest_number(int a, int b, int c);
+/**
+ * main - takes a date and prints how many days are left int the year, taking
+ * leap years into account
+ *
+ * Return: 0
+ */
+int main(void);
+/**
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ *
+ * @month: month in number format
+ *
+ * @day: day of month
+ *
+ * Return: day of year
+ */
+int convert_day(int month, int day);
+/**
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ *
+ * @month: month in number format
+ *
+ * @day: day of month
+ * 
+ * @year: year
+ *
+ * Return: void
+ */
+void print_remaining_days(int month, int day, int year);
 #endif /* MAIN_H */
