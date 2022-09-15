4de994128b011447995afa29f36660349538082e
Wachacha-jay <jameswachacha@gmail.com>
2022-09-15 16:57:21 -0700

    h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplier
+ *
+ * @a: integer to multiply
+ *
+ * @b: integer to multiply
+ *
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
